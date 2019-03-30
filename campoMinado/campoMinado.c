463c0a799cbb575e5f6c898bde1c22aec3c68867
Yuri Becker <becker.yr@gmail.com>
2019-03-30 12:07:29 -0300

    Fix randomNumbers

--- campoMinado/campoMinado.c
+++ campoMinado/campoMinado.c
@@ -12,13 +12,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct matriz {
+typedef struct matriz
+{
   bool bomba;
   bool aberto;
-  int bombasProximas; 
+  int bombasProximas;
 } Matriz;
 
-typedef struct tempo {
+typedef struct tempo
+{
   int dia;
   int mes;
   int ano;
@@ -27,151 +29,162 @@ typedef struct tempo {
   int seg;
 } Tempo;
 
-Matriz** alocarMatriz(int Linhas,int Colunas);
+Matriz **alocarMatriz(int Linhas, int Colunas);
 int getRandomNumber(int max);
-void showMatriz(int linhas, int colunas, Matriz** campo);
+void showMatriz(int linhas, int colunas, Matriz **campo);
 int getInt(int min, int max, char str[]);
 void clearScreen();
-Tempo* getTime();
-void putBombs(Matriz** campo, int linhas, int colunas, int numBombas);
-void initializeMatriz(int linhas, int colunas, Matriz** campo);
+Tempo *getTime();
+void putBombs(Matriz **campo, int linhas, int colunas, int numBombas);
+void initializeMatriz(int linhas, int colunas, Matriz **campo);
 
-int main(){
+int main()
+{
 
   int linhas, colunas, numBombas, totalPosicoes;
-  Matriz** campo; 
-  Tempo* tempoInicial;
+  Matriz **campo;
+  Tempo *tempoInicial;
 
-  tempoInicial = getTime();
+  srand(time(0));
 
   printf("Numero de linhas: ");
   scanf("%d", &linhas);
   printf("Numero de colunas: ");
   scanf("%d", &colunas);
 
-  totalPosicoes = linhas*colunas;
+  totalPosicoes = linhas * colunas;
 
   printf("Numero de Bombas (1-%d) : ", totalPosicoes);
   numBombas = getInt(1, totalPosicoes, "Digite novamente: ");
-  
+
   //Aloca e inicializa a matriz
   campo = alocarMatriz(linhas, colunas);
-
-  if(campo == NULL) return 0;
-  
-  clearScreen();
+  //Verifica se deu certo
+  if (campo == NULL)
+    return 0;
 
   putBombs(campo, linhas, colunas, numBombas);
+  clearScreen();
+  tempoInicial = getTime();
+  showMatriz(linhas, colunas, campo);
 
-  showMatriz(linhas, colunas, campo); 
-    
   //printf("Dia: %d Mes: %d Ano: %d Hora: %d Min: %d Seg: %d\n", tempoInicial->dia, tempoInicial->mes, tempoInicial->ano, tempoInicial->hora, tempoInicial->min, tempoInicial->seg);
-  
- 
+
   return 0;
 }
 
-void initializeMatriz(int linhas, int colunas, Matriz** campo){
-  for(size_t i = 0; i < linhas; i++)
+void initializeMatriz(int linhas, int colunas, Matriz **campo)
+{
+  for (size_t i = 0; i < linhas; i++)
   {
-    for(size_t j = 0; j < colunas; j++)
+    for (size_t j = 0; j < colunas; j++)
     {
       campo[i][j].aberto = false;
-      campo[i][j].bomba= false;
+      campo[i][j].bomba = false;
       campo[i][j].bombasProximas = 0;
     }
-    
   }
-  
 }
 
-void putBombs(Matriz** campo, int linhas, int colunas, int numBombas){
+void putBombs(Matriz **campo, int linhas, int colunas, int numBombas)
+{
   int total = 0;
-  do{
+  do
+  {
     int linha = getRandomNumber(linhas);
     int coluna = getRandomNumber(colunas);
-    if(!campo[linha][coluna].bomba){
+    if (!campo[linha][coluna].bomba)
+    {
       campo[linha][coluna].bomba = true;
       total++;
     }
-  }while(total!=numBombas);
+  } while (total < numBombas);
 }
 
-Tempo* getTime(){
+Tempo *getTime()
+{
   time_t timer;
   struct tm *horarioLocal;
-  Tempo* t = (Tempo*)malloc(sizeof(Tempo)); 
+  Tempo *t = (Tempo *)malloc(sizeof(Tempo));
 
-  time(&timer); // Obtem informações de data e hora
+  time(&timer);                     // Obtem informações de data e hora
   horarioLocal = localtime(&timer); // Converte a hora atual para a hora local
-
 
   t->dia = horarioLocal->tm_mday;
   t->mes = horarioLocal->tm_mon + 1;
   t->ano = horarioLocal->tm_year + 1900;
   t->hora = horarioLocal->tm_hour;
-  t->min  = horarioLocal->tm_min;
-  t->seg  = horarioLocal->tm_sec;
-  
+  t->min = horarioLocal->tm_min;
+  t->seg = horarioLocal->tm_sec;
+
   return t;
 }
 
-void clearScreen(){
+void clearScreen()
+{
   system("@cls||clear");
 }
 
-int getInt(int min, int max, char str[]){
+int getInt(int min, int max, char str[])
+{
   int aux = 0;
   bool errou = false;
-  while(aux > max|| aux < min){
-    if(errou) printf("%s", str);
+  while (aux > max || aux < min)
+  {
+    if (errou)
+      printf("%s", str);
     scanf("%d", &aux);
     errou = true;
   }
   return aux;
 }
 
-void showMatriz(int linhas, int colunas, Matriz** campo){
+void showMatriz(int linhas, int colunas, Matriz **campo)
+{
 
   printf("     ");
-  for(int j = 0; j < colunas; j++){ 
+  for (int j = 0; j < colunas; j++)
+  {
     printf(" %d    ", j);
   }
   printf("\n\n");
-  for(int i = 0; i < linhas; i++)
+  for (int i = 0; i < linhas; i++)
   {
     printf(" %d   ", i);
-    for(int j = 0; j < colunas; j++){ 
+    for (int j = 0; j < colunas; j++)
+    {
       printf("[%d]   ", campo[i][j].bomba);
     }
     printf("\n\n");
   }
-
 }
 
-Matriz** alocarMatriz(int linhas,int colunas){ 
- 
-  Matriz **m = (Matriz**)malloc(linhas * sizeof(Matriz*)); 
- 
-  for (int i = 0; i < linhas; i++){ 
-       m[i] = (Matriz*) malloc(colunas * sizeof(Matriz)); 
+Matriz **alocarMatriz(int linhas, int colunas)
+{
+
+  Matriz **m = (Matriz **)malloc(linhas * sizeof(Matriz *));
+
+  for (int i = 0; i < linhas; i++)
+  {
+    m[i] = (Matriz *)malloc(colunas * sizeof(Matriz));
   }
 
-  if (m==NULL) {
+  if (m == NULL)
+  {
     printf("Memoria insuficiente.\n");
     return m;
   }
 
   initializeMatriz(linhas, colunas, m);
 
-  return m; 
+  return m;
 }
 
-int getRandomNumber(int max){
-  
+int getRandomNumber(int max)
+{
+
   int randomNumber;
-  
-  srand (time(NULL));
+
   randomNumber = rand() % max;
 
   return randomNumber;
