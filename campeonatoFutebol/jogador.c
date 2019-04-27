853914bec4615e64796cceedca37bbd932f7e288
Yuri Becker <becker.yr@gmail.com>
2019-04-27 14:06:29 -0300

    Add jogador.c

---
+++ campeonatoFutebol/jogador.c
@@ -0,0 +1,63 @@
+
+#include "jogador.h"
+
+unsigned int gerarIdJogador()
+{
+  static int ID = 1;
+  return ID++;
+}
+
+// Mostra na tela os dados de um jogador.
+void imprimirJogador(void *jogador)
+{
+  printf("ID: %d - Nome: %s\n", ((Jogador *)jogador)->id, ((Jogador *)jogador)->nome);
+}
+
+// Compara a igualdade entre jogadores.
+bool jogadoresIguais(void *jogador1, void *jogador2)
+{
+  return ((Jogador *)jogador1)->id == ((Jogador *)jogador2)->id;
+}
+
+// Cria um jogador com a id preenchida
+Jogador *criarJogadorPK()
+{
+  Jogador *j = (Jogador *)malloc(sizeof(Jogador));
+  j->nome = (char *)malloc(sizeof(char) * 50);
+  j->id = gerarIdJogador();
+  return j;
+}
+
+// Cria um jogador com a id e o nome preenchidos
+Jogador *criarJogador(const char *nome)
+{
+  Jogador *j = criarJogadorPK();
+  strcpy(j->nome, nome);
+  return j;
+}
+
+// Busca um jogador pela id.
+Jogador *buscarJogador(Lista *jogadores, int id)
+{
+  Jogador *aux = (Jogador *)malloc(sizeof(Jogador));
+  aux->id = id;
+  return (Jogador *)buscar(jogadores, aux, &jogadoresIguais);
+}
+
+// Retira um jogador da lista geral de jogadores através de um ponteiro para este jogador.
+Lista *retirarJogadorP(Lista *jogadores, Jogador *jogador)
+{
+  return retirar(jogadores, jogador, &jogadoresIguais);
+}
+
+// Retira um jogador da lista geral de jogadores através do id
+Lista *retirarJogador(Lista *jogadores, int id)
+{
+  return retirarJogadorP(jogadores, buscarJogador(jogadores, id));
+}
+
+// Imprime a lista de jogadores usando a funcao auxiliar imprimirJogador
+void imprimirJogadores(Lista *jogadores)
+{
+  imprimir(jogadores, &imprimirJogador);
+}
---
+++ campeonatoFutebol/jogador.h
@@ -0,0 +1,28 @@
+#ifndef JOGADOR_H
+#define JOGADOR_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "lista.h"
+
+#define GOLEIRO 0
+#define ATACANTE 1
+#define ZAGUEIRO 2
+
+typedef struct Jogador
+{
+  int id;
+  char *nome;
+  int posicao;
+  int idade;
+  int numCamisa;
+} Jogador;
+
+Jogador *criarJogador(const char *nome);
+Jogador *buscarJogador(Lista *jogadores, int id);
+Lista *retirarJogador(Lista *jogadores, int id);
+Lista *retirarJogadorP(Lista *jogadores, Jogador *jogador);
+void imprimirJogadores(Lista *jogadores);
+
+#endif
