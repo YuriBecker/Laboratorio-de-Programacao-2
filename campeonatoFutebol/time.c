2851c32d73363c6ce722ba55d694e713d2f6f2ca
Yuri Becker <becker.yr@gmail.com>
2019-04-29 20:11:10 -0300

    Add time.c

---
+++ campeonatoFutebol/time.c
@@ -0,0 +1,113 @@
+
+#include "time.h"
+
+/*
+   Funcoes obrigatorias.
+   
+   As funcoes a seguir sao necessarias para parametrizar as funcoes genericas
+   da lista de void. 
+
+*/
+
+// Mostra na tela os dados de um time.
+void imprimirTime(void *time)
+{
+  Time *t = (Time *)time;
+  printf("--------------------------------\n");
+  printf("ID Time: %d - %s\n", t->id, t->nome);
+  if (t->jogadores != NULL)
+  {
+    imprimirJogadores(t->jogadores);
+    printf("--------------------------------\n");
+  }
+}
+
+// Compara a igualdade entre times.
+bool timesIguais(void *time1, void *time2)
+{
+  return ((Time *)time1)->id == ((Time *)time2)->id;
+}
+
+/*
+  Funcoes opcionais.
+  
+  As funcoes a seguir sao auxiliares. Elas foram criadas para facilitar a escrita 
+  do id cliente e para melhorar sua legibilidade.
+  
+  Neste exemplo, todo o id cliente esta no arquivo main.
+*/
+
+unsigned int gerarIdTime()
+{
+  static int ID = 1;
+  return ID++;
+}
+
+// Cria um time com o id preenchido
+Time *criarTimePK()
+{
+  Time *t = (Time *)malloc(sizeof(Time));
+  t->nome = (char *)malloc(sizeof(char) * 50);
+  t->id = gerarIdTime();
+  t->jogadores = criar();
+  t->idTreinador = -1;
+  return t;
+}
+
+// Cria um time com o id e o nome preenchidos
+Time *criarTime(const char *nome)
+{
+  Time *t = criarTimePK();
+  strcpy(t->nome, nome);
+  return t;
+}
+
+/* Em C++ as funcoes criarJogador e criarJogadorPK poderiam ser 
+   sobrecarregadas (terem o mesmo nome) */
+
+// Busca um time pelo código.
+Time *buscarTime(Lista *times, int id)
+{
+  Time *t = (Time *)malloc(sizeof(Time));
+  t->id = id;
+  return (Time *)buscar(times, t, &timesIguais);
+}
+
+// Adiciona um jogador a um time.
+void inscrever(Time *time, Jogador *jogador)
+{
+  jogador->idTime = time->id;
+  time->jogadores = inserirFim(time->jogadores, jogador);
+}
+
+// Retira um jogador de um time.
+void desinscrever(Time *time, int id)
+{
+  Jogador *jogador = buscarJogador(time->jogadores, id);
+  jogador->idTime = -1;
+  time->jogadores = retirarJogadorP(time->jogadores, buscarJogador(time->jogadores, id));
+}
+
+/* Em C++ as funcoes retirarTime e retirarTimeP poderiam ser 
+   sobrecarregadas (terem o mesmo nome) */
+
+// Retira um time através de um ponteiro para este time.
+Lista *retirarTimeP(Lista *times, Time *time)
+{
+  return retirar(times, time, &timesIguais);
+}
+
+// Retira um time através do código do time
+Lista *retirarTime(Lista *times, int id)
+{
+  return retirarTimeP(times, buscarTime(times, id));
+}
+
+/* Em C++ as funcoes retirarJogador e retirarJogadorP poderiam ser 
+   sobrecarregadas (terem o mesmo nome) */
+
+// Imprime a lista de times usando a funcao auxiliar imprimirTime
+void imprimirTimes(Lista *times)
+{
+  imprimir(times, &imprimirTime);
+}
